d00789f8dcf4caa19c0cddb5da7863bb5242c1ba
Tom Victor <vjtomvictor@icloud.com>
2018-10-21 20:16:03 +0530

    testing counter

--- multiscreen/app/src/main/java/com/technorip/kranioz/multi_screen/MainActivity.kt
+++ multiscreen/app/src/main/java/com/technorip/kranioz/multi_screen/MainActivity.kt
@@ -3,7 +3,9 @@ package com.technorip.kranioz.multi_screen
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,8 +15,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun toastMe(view: View){
-    var mytost:Toast = Toast.makeText(this,"I am a Toast",Toast.LENGTH_LONG)
+    var mytost:Toast = Toast.makeText(this,"Toast me?",Toast.LENGTH_LONG)
     mytost.show()
+    }
+
+    fun countMe(view: View){
+        var showCountTextView = findViewById(R.id.counter) as TextView
+        val countString = showCountTextView.text.toString()
+
+        var count: Int = Integer.parseInt(countString)
+        count++
+        showCountTextView.text = count.toString()
     }
 }
 
