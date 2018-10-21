50f3b0aec5de2662e95248d50e764a9f47f2d626
Tom Victor <vjtomvictor@icloud.com>
2018-10-21 22:06:07 +0530

    data transfer with another activity

--- multiscreen/app/src/main/java/com/technorip/kranioz/multi_screen/MainActivity.kt
+++ multiscreen/app/src/main/java/com/technorip/kranioz/multi_screen/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import com.technorip.kranioz.multi_screen.R.string.count
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,15 +24,29 @@ class MainActivity : AppCompatActivity() {
     fun countMe(view: View){
         val showCountTextView = findViewById(R.id.counter) as TextView
         val countString = showCountTextView.text.toString()
-
         var count: Int = Integer.parseInt(countString)
         count++
         showCountTextView.text = count.toString()
     }
-    fun randomMe(view: View){
-        val randomIndent = Intent(this,randon_activity::class.java)
-        startActivity(randomIndent)
 
+    fun randomMe (view: View) {
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, randon_activity::class.java)
+
+        val showCountTextView = findViewById(R.id.counter) as TextView
+        val countStri = showCountTextView.text.toString()
+
+        // Get the current value of the text view.
+        val countString = countStri
+
+        // Convert the count to an int
+        val count = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(randon_activity.TOTAL_COUNT, count)
+
+        // Start the new activity.
+        startActivity(randomIntent)
     }
 }
 
--- multiscreen/app/src/main/java/com/technorip/kranioz/multi_screen/randon_activity.kt
+++ multiscreen/app/src/main/java/com/technorip/kranioz/multi_screen/randon_activity.kt
@@ -2,11 +2,41 @@ package com.technorip.kranioz.multi_screen
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.util.*
+
 
 class randon_activity : AppCompatActivity() {
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_randon_activity)
+        showRandomNumber()
+    }
+
+    fun showRandomNumber() {
+        // Get the count from the intent extras
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        // Generate the random number
+        val random = Random()
+        var randomInt = 0
+        // Add one because the bound is exclusive
+        if (count > 0) {
+            // Add one because the bound is exclusive
+            randomInt = random.nextInt(count + 1)
+        }
+
+        // Display the random number.
+        val textview_random = findViewById(R.id.textview_random) as TextView
+        textview_random.text = Integer.toString(randomInt)
+
+//        // Substitute the max value into the string resource
+//        // for the heading, and update the heading
+//        textview_random.text = getString(R.string.random_heading, count)
     }
 }
